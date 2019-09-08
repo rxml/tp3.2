c93e0c5e49573ec19ce2b935068b75e44c3ff985
rxml <352242180@qq.com>
2019-09-08 12:00:25 +0800

    Update config.php
    sss

--- admin/Conf/config.php
+++ admin/Conf/config.php
@@ -5,10 +5,10 @@ return array(
 	'__PUBLIC__'=>__ROOT__.'/admin/Common/',
 	),
 	'DB_TYPE'   => 'mysql', // 数据库类型
-    'DB_HOST'   => 'qdm161234405.my3w.com', // 服务器地址
-    'DB_NAME'   => 'qdm161234405_db', // 数据库名
-    'DB_USER'   => 'qdm161234405', // 用户名
-    'DB_PWD'    => 'qinchaobin6310', // 密码
+    'DB_HOST'   => 'xxxx.my3w.com', // 服务器地址
+    'DB_NAME'   => 'xxxx', // 数据库名
+    'DB_USER'   => 'xxxx', // 用户名
+    'DB_PWD'    => 'xxxx', // 密码
     'DB_PORT'   => 3306, // 端口
     'DB_PREFIX' => 'think_', // 数据库表前缀 
     'SHOW_ERROR_MSG'=>true,//开启错误调试
